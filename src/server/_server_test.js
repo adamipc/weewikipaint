ea2c945a0d08ab589899dda7e7fbddf523a22db4
Adam Wendt <adam@awendtconsulting.com>
2012-07-14 01:50:06 -0700

    Our first test-driven method

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,7 +1,9 @@
 // Copyright (c) 2012 Adam Wendt Consulting. All rights reserved. See LICENSE.txt for details.
 "use strict";
 
+var server = require("./server.js");
+
 exports.testNothing = function(test) {
-    test.ok(true, "Hello");
+    test.equals(3, server.number(), "number");
     test.done();
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,3 +1,6 @@
 // Copyright (c) 2012 Adam Wendt Consulting. All rights reserved. See LICENSE.txt for details.
+"use strict";
 
-console.log("Hello, I'm a server. No, really. What?");
+exports.number = function() {
+ return 3;
+};

400f3900c88c3ae00f474714bd3d7f8f21d1e8b1
Adam Wendt <adam@awendtconsulting.com>
2012-07-14 01:40:35 -0700

    Nodeunit test runner is working

--- jakefile.js
+++ jakefile.js
@@ -18,7 +18,8 @@
 
     desc("Test everything.");
     task("test", [], function() {
-        console.log("test goes here");
+        var reporter = require("nodeunit").reporters["default"];
+        reporter.run(['test']);
     });
 
     desc("Integrate");

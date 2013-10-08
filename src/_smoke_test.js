dc7fda74412126fd3322965fa0d66c30be8cf8b9
Adam Wendt <adam@awendtconsulting.com>
2013-10-08 13:25:05 -0700

    Updated todo, started smoke test file

---
+++ src/_smoke_test.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2012 Adam Wendt Consulting. All rights reserved. See LICENSE.txt for details.
+
+// launch the server in same way it happens in production
+// get a page
+// confirm we got something
